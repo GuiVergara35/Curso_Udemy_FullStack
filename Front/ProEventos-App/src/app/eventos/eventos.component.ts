7f4b41cb3099cd697eab6d62b914e255fbe04852
gedg <guilherme.vergara@gft.com>
2021-07-30 11:10:25 -0300

    Novo commit com layout em Angular

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -9,15 +9,46 @@ import { Component, OnInit } from '@angular/core';
 export class EventosComponent implements OnInit {
 
   public eventos: any;
+  public eventosFiltrados: any = [];
+
+  widthImg: number = 150;
+  marginImg: number =3;
+  showImg: boolean = true;
+  private _filterList: string = '';
+
+public get filterList(): string {
+  return this._filterList;
+}
+
+public set filterList(value: string){
+  this._filterList = value;
+  this.eventosFiltrados = this.filterList ? this.filterEvents(this.filterList) : this.eventos;
+}
+
+filterEvents(filterBy: string): any {
+  filterBy = filterBy.toLocaleLowerCase();
+  return this.eventos.filter(
+    (evento: { tema: string; local: string; }) => evento.tema.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
+    evento.local.toLocaleLowerCase().indexOf(filterBy) !== -1
+  );
+}
+
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getEventos();
   }
 
+  hideImg(){
+    this.showImg = !this.showImg;
+  }
+
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/v1/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
       error => console.log(error)
     );
   }
